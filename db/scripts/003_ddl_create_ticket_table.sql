13fdc9c5d2bfa77c73f6972cecf637ce5de93f8c
diac <nikolai.gladkikh@gmail.com>
2022-11-09 17:50:50 +0300

    Исправить ограничение уникальности в таблице ticket

--- db/scripts/003_ddl_create_ticket_table.sql
+++ db/scripts/003_ddl_create_ticket_table.sql
@@ -4,8 +4,7 @@ CREATE TABLE ticket (
     pos_row INT NOT NULL,
     cell INT NOT NULL,
     user_id INT NOT NULL REFERENCES users(id),
-    CONSTRAINT UTicket_Session_User UNIQUE (session_id, user_id),
-    CONSTRAINT UTicket_Place UNIQUE (pos_row, cell)
+    CONSTRAINT UTicket_Session_Place UNIQUE (session_id, pos_row, cell)
 );
 
 COMMENT ON TABLE ticket IS 'Билеты. Каждый билет связывает пользователя с сеансом.';
