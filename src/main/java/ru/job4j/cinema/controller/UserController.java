13eb0cdccf5133bade93574ba8f2434a715754aa
diac <nikolai.gladkikh@gmail.com>
2022-11-11 13:49:42 +0300

    Добавить страницу и действие входа в систему

---
+++ src/main/java/ru/job4j/cinema/controller/UserController.java
@@ -0,0 +1,43 @@
+package ru.job4j.cinema.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.cinema.model.User;
+import ru.job4j.cinema.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+@ThreadSafe
+public final class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("fail", fail != null);
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, HttpServletRequest req) {
+        Optional<User> userInDb = userService.findByUsername(user.getUsername());
+        if (userInDb.isEmpty()) {
+            return "redirect:/login?fail=true";
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("user", userInDb.get());
+        return "redirect:/";
+    }
+}
