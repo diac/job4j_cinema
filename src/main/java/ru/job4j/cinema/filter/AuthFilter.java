d7a20138b519c3527c6646aebc2b50dcf17d424e
diac <nikolai.gladkikh@gmail.com>
2022-11-11 13:45:20 +0300

    Добавить фильтр авторизации

---
+++ src/main/java/ru/job4j/cinema/filter/AuthFilter.java
@@ -0,0 +1,40 @@
+package ru.job4j.cinema.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class AuthFilter implements Filter {
+
+    private static final Set<String> ALLOWED_MAPPINGS = Set.of(
+            "login"
+    );
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (allowGuestAccess(uri)) {
+            chain.doFilter(req, res);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+
+    private boolean allowGuestAccess(String uri) {
+        return ALLOWED_MAPPINGS.stream().anyMatch(uri::endsWith);
+    }
+}
