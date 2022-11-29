fb28d34966f6fda9f2624f55f19d08d57df779b3
diac <nikolai.gladkikh@gmail.com>
2022-11-29 15:43:13 +0300

    Исправить опечатку

--- src/main/java/ru/job4j/cinema/service/SimpleHallService.java
+++ src/main/java/ru/job4j/cinema/service/SimpleHallService.java
@@ -6,7 +6,7 @@ import ru.job4j.cinema.model.Hall;
 import java.util.List;
 
 @Service
-public  class SimpleHallService implements HallService {
+public class SimpleHallService implements HallService {
 
     public Hall getHall() {
         return new Hall(List.of(5, 10, 5, 12, 12));
